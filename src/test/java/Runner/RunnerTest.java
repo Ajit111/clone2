c2eed730a9433c854013f847250acdc527ae1ffb
Ajit kumar <59956826+Ajit111@users.noreply.github.com>
2025-09-25 18:39:59 +0530

    Update RunnerTest.java

--- src/test/java/Runner/RunnerTest.java
+++ src/test/java/Runner/RunnerTest.java
@@ -21,18 +21,27 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
+// @CucumberOptions(
+//     features = "src/test/java/Features",    
+//     glue = {"StepDefinition"},               
+//     plugin = {
+//         "pretty",
+//         "json:target/cucumber.json",         
+//         "html:target/cucumber-html-report",  
+//         "junit:target/cucumber.xml"          
+//     },
+//     monochrome = true,
+//     publish = true
+// )
 @CucumberOptions(
-    features = "src/test/java/Features",    
-    glue = {"StepDefinition"},               
-    plugin = {
-        "pretty",
-        "json:target/cucumber.json",         
-        "html:target/cucumber-html-report",  
-        "junit:target/cucumber.xml"          
-    },
-    monochrome = true,
-    publish = true
+    features = "src/test/java/Features",
+    glue = {"StepDefinition"},
+    plugin = {"pretty", "json:target/cucumber.json", "html:target/cucumber-html-report"},
+    monochrome = true
 )
+
+
+    
 public class RunnerTest {
 }
 
