45fc5147d9fa28218055454ca00ba5a9921c0b17
Ajit kumar <59956826+Ajit111@users.noreply.github.com>
2025-09-25 19:40:44 +0530

    Update RunnerTest.java

--- src/test/java/Runner/RunnerTest.java
+++ src/test/java/Runner/RunnerTest.java
@@ -1,19 +1,3 @@
-// package Runner;
-
-// import org.junit.runner.RunWith;
-
-// import io.cucumber.junit.Cucumber;
-// import io.cucumber.junit.CucumberOptions;
-
-// @RunWith(Cucumber.class)
-// @CucumberOptions(features="src/test/java/Features", tags="@login",glue = {"StepDefinition","base"},
-// plugin = {"pretty", "html:target/HTML-reports/test.html","json:target/cucumber-report/cucumber.json"})
-
-
-// public class RunnerTest {
-
-// }
-
 package Runner;
 
 import org.junit.runner.RunWith;
@@ -21,27 +5,17 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-// @CucumberOptions(
-//     features = "src/test/java/Features",    
-//     glue = {"StepDefinition"},               
-//     plugin = {
-//         "pretty",
-//         "json:target/cucumber.json",         
-//         "html:target/cucumber-html-report",  
-//         "junit:target/cucumber.xml"          
-//     },
-//     monochrome = true,
-//     publish = true
-// )
 @CucumberOptions(
     features = "src/test/java/Features",
     glue = {"StepDefinition"},
-    plugin = {"pretty", "json:target/cucumber.json", "html:target/cucumber-html-report"},
-    monochrome = true
+    plugin = {
+        "pretty",
+        "json:target/cucumber.json",
+        "html:target/cucumber-html-report"
+    },
+    monochrome = true,
+    tags = "@login"
 )
-
-
-    
 public class RunnerTest {
+    // JSON always generate hota hai, agar tests fail bhi ho to empty file create hoti hai
 }
-
